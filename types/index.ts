dcc6da12fd774119df466160782468163123b708
lucas <accaunoff99@gmail.com>
2025-04-10 19:20:09 +0900

    before rm-rf

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,11 @@
 export interface ChildProps{
     children: React.ReactNode;
 }
+export interface IBlog{
+    title: string;
+    description: string;
+    date: string;
+    author:string,
+    image: string;
+    tags:string[]
+}
